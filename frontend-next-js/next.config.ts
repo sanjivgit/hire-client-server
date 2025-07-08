952abad21b151584eb462dc84eee03e05b9a6b4a
sanjiv kumar <sanjivkumarhzb1427@gmail.com>
2025-07-08 08:56:52 +0530

    final project

--- frontend-next-js/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export", // Use static export
-  trailingSlash: true, // Helps with routing
-};
-
-export default nextConfig;
